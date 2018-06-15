3c1f6b3abe68f05437a89957f0826b4687872979
Craig Gordon <craigmgordon@gmail.com>
2018-06-15 13:15:09 +0100

    phase2

---
+++ creditcheckservice-producer/src/main/java/com/example/creditcheckservice/CreditcheckserviceProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.creditcheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcheckserviceProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcheckserviceProducerApplication.class, args);
+	}
+}
